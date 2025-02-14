073a760af5f039480e3e73f61330e68c96cc8833
princesharmapd <princesharma1153@outlook.com>
2025-02-14 21:19:24 +0530

    fresh react vite

--- src/main.jsx
+++ src/main.jsx
@@ -1,35 +1,10 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import HomePage from "./pages/HomePage";
-import ContactPage from "./pages/ContactPage";
-import RoomPage from "./pages/RoomsPage";
-import NoPageFound from "./pages/NoPageFound";
-import "./index.css";
-import { DataProvider } from "./components/global/DataContext";
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
 
-const router = createBrowserRouter([
-    {
-        path: "/",
-        element: <HomePage />,
-        errorElement: <NoPageFound />,
-    },
-
-    {
-        path: "/rooms",
-        element: <RoomPage />,
-    },
-
-    {
-        path: "/contact",
-        element: <ContactPage />,
-    },
-]);
-
-ReactDOM.createRoot(document.getElementById("root")).render(
-    <React.StrictMode>
-    <DataProvider>
-        <RouterProvider router={router} />
-    </DataProvider>
-    </React.StrictMode >
-);
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
