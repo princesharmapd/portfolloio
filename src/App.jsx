b3087423c022aa57d115f60f70ebe9b55bcd615f
princesharmapd <princesharma1153@outlook.com>
2025-02-21 22:45:50 +0530

    updated the cnges

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,35 @@
-import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-// import AuthProvider from "./context/AuthContext";
-import LandingPage from "./Pages/LandingPage";
-import Dashboard from "./Pages/Dashboard";
-import Topbar from "./components/TopBar";
-import NotFound from "./Pages/NotFound";
-import ServerDown from "./Pages/ServerDown";
-import Unauthorized from "./Pages/Unauthorized";
-import MarketOverview from "./Pages/MarketOverview";
-// import Callback from "./Pages/Callback";
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
 
-const App = () => {
+function App() {
+  const [count, setCount] = useState(0)
+
   return (
-    // <AuthProvider>
-      <Router>
-        <div style={{ display: "flex", flexDirection: "column"}}>
-          <Topbar />
-          <Routes>
-            <Route path="portfolloio//" element={<LandingPage />} />
-            {/* <Route path="/auth/callback" element={<Callback />} /> */}
-            <Route path="/dashboard" element={<Dashboard />} />
-            <Route path="/executive" element={<MarketOverview />} />
+    <>
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
 
-            <Route path="/server-down" element={<ServerDown />} />
-            <Route path="/unauthorized" element={<Unauthorized />} />
-            <Route path="*" element={<NotFound />} />
-          </Routes>
-        </div>
-      </Router>
-    // </AuthProvider>
-  );
-};
-
-export default App;
+export default App
